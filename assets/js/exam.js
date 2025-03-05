8dfb5df6de679677c5b799160cb4e39f0c756fa1
senna <kobasena1007@gmail.com>
2025-03-05 21:37:33 +0800

    Update exam.js

--- assets/js/exam.js
+++ assets/js/exam.js
@@ -47,7 +47,7 @@ $("#end-exam").on("click",function(){
 
 function justPreview(){
     if(nickname){
-        window.location.href=`../../member.html?nickname=${encodeURIComponent(nickname)}&score=${score}`;
+        window.location.href=`./member.html?nickname=${encodeURIComponent(nickname)}&score=${score}`;
     }else{
         $(".alert").show();
     }
@@ -70,7 +70,7 @@ async function sendData() {
         });
         console.log("データを送信しました！");
         inputElement.value = "";
-        window.location.href=`../member.html?nickname=${encodeURIComponent(nickname)}&score=${encodeURIComponent(score)}`;
+        window.location.href=`./member.html?nickname=${encodeURIComponent(nickname)}&score=${encodeURIComponent(score)}`;
     } catch (error) {
         console.error("データ送信エラー:", error);
     }
